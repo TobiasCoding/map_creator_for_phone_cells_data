e37b28b353f7b80710f959b360f0a249c42010db
TobiasCoding <86751087+TobiasCoding@users.noreply.github.com>
2024-12-10 14:10:37 -0300

    Update create_map.py

--- create_map.py
+++ create_map.py
@@ -3,39 +3,33 @@ import folium
 from folium.plugins import MarkerCluster
 from IPython.display import display
 
-# Leer el archivo de Excel creado anteriormente
 impactos = pd.read_excel('Impactos_con_celdas.xlsx')
 
-# Convertir las columnas de latitud y longitud a numéricas, forzando errores a NaN
 impactos['Latitud'] = pd.to_numeric(impactos['Latitud'], errors='coerce')
 impactos['Longitud'] = pd.to_numeric(impactos['Longitud'], errors='coerce')
 
-# Eliminar filas con valores NaN en las columnas de latitud y longitud
 impactos = impactos.dropna(subset=['Latitud', 'Longitud'])
 
-# Crear un mapa centrado en la primera coordenada válida
 if not impactos.empty:
     m = folium.Map(location=[impactos['Latitud'].mean(), impactos['Longitud'].mean()], zoom_start=10)
 
-    # Crear un cluster de marcadores
     marker_cluster = MarkerCluster().add_to(m)
 
-    # Añadir círculos para cada impacto al cluster de marcadores
     for _, row in impactos.iterrows():
         try:
           radio = int(row['Radio'])*1000
           folium.Circle(
               location=[row['Latitud'], row['Longitud']],
-              radius=radio,  # Convertir km a metros  / Puede cambiar y llamarse "Radio_Cobertura_en_KM" según ISP
+              radius=radio,  # Convertir km a metros  / Puede cambiar y llamarse "Radio_Cobertura_en_KM" según proveedor
               popup=(
                   f"Fecha: {row['Fecha']}<br>"
-                  f"Evento: {row['Evento']}<br>"    # Puede cambiar a segun ISP:    f"Hora: {row['Hora']}<br>"
-                  f"Nro. Pagador: {row['Nro. Pagador']}<br>"
-                  f"Dirección Evento: {row['Dirección Evento']}<br>"
-                  f"Duración: {row['Duración en Seg.']}<br>"
+                  f"Evento: {row['Evento']}<br>"    # Puede cambiar a segun proveedor: "Hora"
+                  f"Nro. Pagador: {row['Nro. Pagador']}<br>" # Puede cambiar a segun proveedor
+                  f"Dirección Evento: {row['Dirección Evento']}<br>" # Puede cambiar a segun proveedor
+                  f"Duración: {row['Duración en Seg.']}<br>" # Puede cambiar a segun proveedor
                   f"IMEI: {row['IMEI']}<br>"
-                  f"Tipo: {row['Tipo Tráfico']}<br>"
-                  f"Estado: {row['Estado']}"
+                  f"Tipo: {row['Tipo Tráfico']}<br>"  # Puede cambiar a segun proveedor
+                  f"Estado: {row['Estado']}" # Puede cambiar a segun proveedor
               ),
               color='blue',
               fill=True,
